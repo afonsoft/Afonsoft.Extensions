687b2e8438ac1304e9b64a3c5194ccc1f028acf1
Afonso Dutra Nogueira Filho <afonso.nogueira@outlook.com.br>
2018-02-06 11:48:00 -0200

    First Upload

---
+++ Afonsoft.Extensions/ExceptionExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+/// <summary>
+/// Classe com extenção para EXCEPTION 
+/// AFONSO DUTRA NOGUEIRA FILHO
+/// </summary>
+public static class ExceptionExtensions
+{
+    private static readonly object Locker = new object();
+
+    /// <summary>
+    /// Tratar a Exception para recuperar um string com todos o erro detalhado.
+    /// </summary>
+    public static string Treatment(this Exception exception)
+    {
+        string msgRetorno = "";
+
+        string exceptionMessages = string.Empty;
+        string stackTraces = string.Empty;
+        Exception tmpException = exception;
+
+        if (tmpException != null)
+        {
+            while (tmpException != null)
+            {
+                exceptionMessages += tmpException.Message + Environment.NewLine;
+                string traces = "";
+                var trace = new StackTrace(tmpException, true);
+                var stackFrames = trace.GetFrames();
+                if (stackFrames != null)
+                    foreach (StackFrame stack in stackFrames)
+                    {
+                        if (stack.GetFileLineNumber() > 0)
+                        {
+                            traces += "FileName: " + stack.GetFileName() + Environment.NewLine;
+                            traces += "Metodo: " + stack.GetMethod().Name + Environment.NewLine;
+                            traces += "Line: " + stack.GetFileLineNumber() + Environment.NewLine;
+                            traces += "Column: " + stack.GetFileColumnNumber() + Environment.NewLine + Environment.NewLine;
+                        }
+                    }
+                if (!String.IsNullOrEmpty(traces))
+                    stackTraces += tmpException.StackTrace + Environment.NewLine + Environment.NewLine + traces + Environment.NewLine + Environment.NewLine;
+                else
+                    stackTraces += tmpException.StackTrace + Environment.NewLine + Environment.NewLine;
+
+                tmpException = tmpException.InnerException;
+            }
+            msgRetorno = "Messages: " + exceptionMessages + Environment.NewLine + " StackTraces: " + stackTraces;
+
+            Trace.WriteLine(msgRetorno);
+            Debug.WriteLine(msgRetorno);
+        }
+        return msgRetorno;
+    }
+}
---
+++ Afonsoft.Extensions/ObjectExtensions.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+    /// <summary>
+    /// Classe interna para trabalhar com UFT8
+    /// </summary>
+    internal class Utf8StringWriter : StringWriter
+    {
+        public override Encoding Encoding { get { return Encoding.UTF8; } }
+    }
+
+/// <summary>
+/// Classe com extenção para OBJECT 
+/// AFONSO DUTRA NOGUEIRA FILHO
+/// </summary>
+public static class ObjectExtensions
+{
+    /// <summary>
+    /// Metodo para tratar o Null para Empty ou aspas
+    /// </summary>
+    /// <param name="Value"></param>
+    /// <returns>Value ou Empty</returns>
+    public static string NullToString(this object Value)
+    {
+        return Value == null ? "" : Value.ToString();
+    }
+
+    /// <summary>
+    /// Serializar o Objeto - Criar o XML
+    /// </summary>
+    /// <typeparam name="T">Tipo do Objeto</typeparam>
+    /// <param name="toSerialize">Objeto a Serializar</param>
+    /// <returns>XML</returns>
+    public static string SerializeXml<T>(this T toSerialize)
+    {
+        try
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(toSerialize.GetType());
+            using (StringWriter textWriter = new Utf8StringWriter())
+            {
+                xmlSerializer.Serialize(textWriter, toSerialize);
+                return textWriter.ToString();
+            }
+        }
+        catch
+        {
+            return "";
+        }
+    }
+
+    /// <summary>
+    /// Serializar o Objeto - Criar o Json
+    /// </summary>
+    /// <typeparam name="T">Tipo do Objeto</typeparam>
+    /// <param name="toSerialize">Objeto a Serializar</param>
+    /// <returns>JSON</returns>
+    public static string SerializeJson<T>(this T toSerialize)
+    {
+        try
+        {
+            return JsonConvert
+                .SerializeObject(toSerialize, Formatting.None,
+                    new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore })
+                .Replace(Environment.NewLine, "")
+                .Replace("\n", "")
+                .Replace("\r", "");
+        }
+        catch
+        {
+            return JsonConvert.SerializeObject(toSerialize);
+        }
+    }
+
+    /// <summary>
+    /// Deserialize a string Json para um objeto
+    /// </summary>
+    /// <typeparam name="T">Tipo do objeto</typeparam>
+    /// <param name="json">String em json</param>
+    /// <returns>retorna o objeto preenchido</returns>
+    public static T DeserializeJson<T>(this string json) where T : class
+    {
+        if (string.IsNullOrEmpty(json)) return default(T);
+        try
+        {
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+        catch (Exception)
+        {
+            return default(T);
+        }
+    }
+
+    /// <summary>
+    /// Deserialize a string xml para um objeto
+    /// </summary>
+    /// <typeparam name="T">Tipo do objeto</typeparam>
+    /// <param name="xml">String em XML</param>
+    /// <returns>retorna o objeto preenchido</returns>
+    public static T DeserializeXml<T>(this string xml) where T : class
+    {
+        if (string.IsNullOrEmpty(xml)) return default(T);
+        try
+        {
+            StreamReader xmlStream = new StreamReader(xml);
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            return (T)serializer.Deserialize(xmlStream);
+        }
+        catch (Exception)
+        {
+            return default(T);
+        }
+    }
+}
