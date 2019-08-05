416bcf65d96526bdb6e499322d8b0cde26afdc77
Afonso Dutra Nogueira Filho <mzt.adnfilho@voegol.com.br>
2019-08-05 15:55:58 -0300

    DataReaderExtensions

---
+++ Afonsoft.Extensions/DataReaderExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
+using System.Text;
+
+namespace Afonsoft.Extensions
+{
+   public static class DataReaderExtensions
+    {
+        /// <summary>
+        /// DataReaderMapToList
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="dr"></param>
+        /// <returns></returns>
+        public static List<T> DataReaderMapToList<T>(this IDataReader dr)
+        {
+            List<T> list = new List<T>();
+            T obj = default(T);
+            while (dr.Read())
+            {
+                obj = Activator.CreateInstance<T>();
+                foreach (PropertyInfo prop in obj.GetType().GetProperties())
+                {
+                    if (!object.Equals(dr[prop.Name], DBNull.Value))
+                    {
+                        prop.SetValue(obj, dr[prop.Name], null);
+                    }
+                }
+                list.Add(obj);
+            }
+            return list;
+        }
+
+        /// <summary>
+        /// Select
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="reader"></param>
+        /// <param name="projection"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> Select<T>(this IDataReader reader, Func<IDataReader, T> projection)
+        {
+            while (reader.Read())
+            {
+                yield return projection(reader);
+            }
+        }
+    }
+}
