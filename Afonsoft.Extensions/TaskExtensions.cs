98f04a99715e2d0f4b68b4345774bc50a0ea1313
Afonso Dutra Nogueira Filho <afonsoft@gmail.com>
2020-02-20 12:34:06 -0300

    Implementação da TASK

---
+++ Afonsoft.Extensions/TaskExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Afonsoft.Extensions
+{
+    public static class TaskExtensions
+    {
+        //https://stackoverflow.com/questions/10134310/how-to-cancel-a-task-in-await
+        public static async Task<T> WaitOrCancel<T>(this Task<T> task, CancellationToken token)
+        {
+            token.ThrowIfCancellationRequested();
+            await Task.WhenAny(task, token.WhenCanceled());
+            token.ThrowIfCancellationRequested();
+
+            return await task;
+        }
+
+        public static Task WhenCanceled(this CancellationToken cancellationToken)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            cancellationToken.Register(s => ((TaskCompletionSource<bool>)s).SetResult(true), tcs);
+            return tcs.Task;
+        }
+    }
+}
