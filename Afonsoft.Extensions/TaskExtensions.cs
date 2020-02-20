73e216f17b1a418662d67e36127dc740fee9e2fc
Afonso Dutra Nogueira Filho <afonsoft@gmail.com>
2020-02-20 15:16:08 -0300

    Correções das Versões

--- Afonsoft.Extensions/TaskExtensions.cs
+++ Afonsoft.Extensions/TaskExtensions.cs
@@ -6,13 +6,26 @@ namespace Afonsoft.Extensions
     public static class TaskExtensions
     {
         //https://stackoverflow.com/questions/10134310/how-to-cancel-a-task-in-await
-        public static async Task<T> WaitOrCancel<T>(this Task<T> task, CancellationToken token)
+
+#if NET40
+        public static Task<T> WaitOrCancel<T>(this Task<T> task, CancellationToken token)
+
+#else
+        public async static Task<T> WaitOrCancel<T>(this Task<T> task, CancellationToken token)
+#endif
         {
             token.ThrowIfCancellationRequested();
+#if NET40
+            Task.WaitAny(task, token.WhenCanceled());
+#else
             await Task.WhenAny(task, token.WhenCanceled());
+#endif
             token.ThrowIfCancellationRequested();
-
+#if NET40
+            return task;
+#else
             return await task;
+#endif
         }
 
         public static Task WhenCanceled(this CancellationToken cancellationToken)
